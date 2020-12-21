90dba2a9ff71d080493ab460a0c3214e07c3622d
varma1999 <71266779+varma1999@users.noreply.github.com>
2020-12-21 04:21:41 -0400

    Lab 3(adjusted accordingly)

---
+++ Lab 3 Testing/hello_world_main.c
@@ -0,0 +1,43 @@
+/* Hello World Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+
+void app_main()
+{
+	//void unit_test() //This was used to split the program into two different functions
+	//{
+		printf("Hello World \n"); //Unit Test 1- the commented code was adjusted to facilitate unit and integration Testing 
+		//printf("My name is Varma Ramlochansingh and my ID number is 816008167 \n")
+		//printf("My name is Varma Ramlochansingh \n")
+		//printf("")
+	//}
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+    printf("This is ESP8266 chip with %d CPU cores, WiFi, ",
+            chip_info.cores);
+
+    printf("silicon revision %d, ", chip_info.revision);
+
+    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    for (int i = 10; i >= 0; i--) { //Unit Test 2- the value of i(n) was changed. Values used were -7,0,7 and 10
+        printf("Restarting in %d seconds...\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    printf("Restarting now.\n");
+    fflush(stdout);
+    esp_restart();
+}
